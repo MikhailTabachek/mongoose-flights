f64bdd71ee142ddd1b551e0f15f1d24c5437103b
Mikhail <mishanyaveselyi@gmail.com>
2022-03-02 20:54:46 -0500

    Set Option to Edit Flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,23 +1,30 @@
 import { Flight } from "../models/flight.js"
 
-function newFlight(req, res) {
-  const newFlight = new Flight()
-  const dt = newFlight.departs
-  const departsDate = dt.toISOString().slice(0,16)
-  res.render('flights/new', {departsDate})
-}
 
 function index (req, res){
   Flight.find({}, function(error, flights){
     res.render('flights/index', {
-      flights,
       error,
+      flights,
       title: "All Flights"
     })
   })
 }
 
+function newFlight(req, res) {
+  const newFlight = new Flight()
+  const dt = newFlight.departs
+  const departsDate = dt.toISOString().slice(0,16)
+  res.render('flights/new', {
+    departsDate,
+    title: "Add Flight"
+  })
+}
+
 function create(req, res){
+  if (req.body.departs === ""){
+    delete req.body.departs
+  }
   const flight = new Flight(req.body)
   flight.save(function(err){
     if(err) return res.redirect('/flights/new')
@@ -26,8 +33,43 @@ function create(req, res){
   console.log(req.body);
 }
 
+function show(req, res){
+  Flight.findById(req.params.id, function(err, flight){
+    res.render('flights/show', {
+      title: "Flight Details",
+      flight
+    })
+  })
+}
+
+function deleteFlight(req, res){
+  Flight.findByIdAndDelete(req.params.id, function(err, flight){
+    res.redirect('/flights')
+  })
+}
+
+function edit(req, res){
+  Flight.findById(req.params.id, function(err, flight){
+    res.render('flights/edit', {
+      flight,
+      err,
+      title:"Edit Your Flight"
+    })
+  })
+}
+
+function update(req, res){
+  Flight.findByIdAndUpdate(req.params.id, req.body, function(err, flight){
+    res.redirect(`/flights/${flight._id}`)
+  })
+}
+
 export {
   newFlight as new,
   create,
-  index
+  index,
+  show,
+  deleteFlight as delete,
+  edit,
+  update
 }
--- routes/flights.js
+++ routes/flights.js
@@ -1,11 +1,16 @@
 import { Router } from 'express'
+import methodOverride from 'method-override'
 const router = Router()
 import * as flightsCtrl from '../controllers/flights.js'
 
 /* GET flights listing. */
 router.get('/new', flightsCtrl.new)
 router.get('/', flightsCtrl.index)
+router.get('/:id', flightsCtrl.show)
+router.get('/:id/edit', flightsCtrl.edit)
 router.post('/', flightsCtrl.create)
+router.delete('/:id', flightsCtrl.delete)
+router.put('/:id', flightsCtrl.update)
 
 export {
   router
